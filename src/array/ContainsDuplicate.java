a341da7e0dc1a37839ccdb1e4092233b43a6de33
mohamedjamal0 <mohamedjamal9810@gmail.com>
2024-03-10 00:22:37 +0300

    contains duplicate

---
+++ src/array/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashMap<Integer, Boolean> hashmap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int number = nums[i];
+            if (hashmap.containsKey(number)) return true;
+            hashmap.put(number, true);
+
+        }
+
+
+        return false;
+    }
+}
